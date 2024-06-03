2745bba738637a23a67f0f33c35b7dab0260c192
Harris Beg <harris@harrisbeg.com>
2024-06-03 12:28:16 -0700

    Add current API

---
+++ app.js
@@ -0,0 +1,273 @@
+"use strict";
+// 1. Load required modules
+const express = require("express");
+const multer = require("multer");
+const mysql = require("promise-mysql");
+const config = require("./config.js");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+const SERVER_ERROR = "Something went wrong on the server, please try again later.";
+const SERVER_ERR_CODE = 500;
+const CLIENT_ERR_CODE = 400;
+const DEBUG = true;
+
+// Cookie expires in 15 minutes
+const COOKIE_EXP_TIME = 15 * 60 * 1000;
+
+// for application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true })); // built-in middleware
+// for application/json
+app.use(express.json()); // built-in middleware
+// for multipart/form-data (required with FormData)
+app.use(multer().none()); // requires the "multer" module
+app.use(cookieParser()); // requires the "cookie parser"
+app.use(express.static("public"));
+
+/**
+ * Establishes a database connection to the storedb and returns the database object.
+ * Any errors that occur during connection should be caught in the function
+ * that calls this one.
+ * @returns {Object} - The database object for the connection.
+ */
+async function getDB() {
+    const db = await mysql.createConnection({
+      // Variables for connections to the database.
+      host: config.HOST,
+      port: config.PORT,
+      user: config.USER,
+      password: config.PASSWORD,
+      database: config.DATABASE
+    });
+    return db;
+}
+
+// GET endpoints
+
+app.get('/products', async (req, res, next) => {
+    let storeName = req.query.store_name;
+    if (!(storeName)) {
+        res.status(CLIENT_ERR_CODE); // re-route to errorHandler, exiting this function
+        next(Error("Missing GET parameter: store name."));
+    }
+    else {
+        let db;
+        try {
+            db = await getDB(); // connection error thrown in getDB();
+            let selectFields = "id, category, title, price, quantity";
+            let qry = `SELECT ${selectFields} FROM products WHERE store_name=?;`;
+            let result = await db.query(qry, [storeName.toLowerCase()]);
+            res.json(result);
+        } catch (err) {
+            res.status(SERVER_ERR_CODE);
+            err.message = SERVER_ERROR;
+            next(err);
+        }
+        if (db) { // only defined if getDB() returned a successfully-connected object
+            db.end();
+        }
+    }
+});
+
+app.get('/products/category', async (req, res, next) => {
+    let storeName = req.query.store_name;
+    let category = req.query.category;
+    if (!(storeName)) {
+        res.status(CLIENT_ERR_CODE); // re-route to errorHandler, exiting this function
+        next(Error("Missing GET parameter: store name."));
+    }
+    else {
+        let db;
+        try {
+            db = await getDB(); // connection error thrown in getDB();
+            let selectFields = "id, category, title, price, quantity";
+            let qry = `SELECT ${selectFields} FROM products WHERE store_name=? AND category=?;`;
+            let result = await db.query(qry, [storeName.toLowerCase(), category]);
+            res.json(result);
+        } catch (err) {
+            res.status(SERVER_ERR_CODE);
+            err.message = SERVER_ERROR;
+            next(err);
+        }
+        if (db) { // only defined if getDB() returned a successfully-connected object
+            db.end();
+        }
+    }
+});
+
+app.get('/categories', async (req, res, next) => {
+    let storeName = req.query.store_name;
+    if (!(storeName)) {
+        res.status(CLIENT_ERR_CODE); // re-route to errorHandler, exiting this function
+        next(Error("Missing GET parameter: store name."));
+    }
+    else {
+        let db;
+        try {
+            db = await getDB(); // connection error thrown in getDB();
+            let qry = `SELECT DISTINCT category FROM products WHERE store_name=?;`;
+            let result = await db.query(qry, [storeName.toLowerCase()]);
+            res.json(result);
+        } catch (err) {
+            res.status(SERVER_ERR_CODE);
+            err.message = SERVER_ERROR;
+            next(err);
+        }
+        if (db) { // only defined if getDB() returned a successfully-connected object
+            db.end();
+        }
+    }
+});
+
+app.get('/admin/isloggedin', async (req, res, next) => {
+    let loggedIn = false;
+    if (req.cookies["logged_in"]) {
+        loggedIn = true;
+    }
+    res.json({
+        "logged_in": loggedIn
+    });
+});
+
+app.get('/admin/logout', async (req, res) => {
+    res.clearCookie("logged_in");
+    res.redirect("/");
+});
+
+// POST endpoints
+
+app.post('/admin/product/create', async (req, res, next) => {
+    let storeName = req.body.store_name
+    let productTitle = req.body.title;
+    let productDescription = req.body.description;
+    let productPrice = req.body.price;
+    let productQty = req.body.quantity;
+    let productCategory = req.body.category;
+
+    if (!(storeName && productDescription && productCategory &&
+          productTitle && productPrice && productQty)) {
+        res.status(CLIENT_ERR_CODE); // re-route to errorHandler, exiting this function
+        next(Error("Missing POST parameter: store name, title, description, " +
+                   "price, quantity, and/or category."));
+    }
+    let db;
+    try {
+        db = await getDB(); // connection error thrown in getDB();
+        let insertFields = "(store_name, category, title, description, price, quantity)";
+        let qry = `INSERT INTO products ${insertFields} VALUES (?, ?, ?, ?, ?, ?);`;
+        await db.query(qry, [storeName, productCategory, productTitle,
+                             productDescription, productPrice, productQty]);
+        res.json({"status_message": `Request to add ${productTitle} to ${storeName} ` +
+                 `successfully processed!`});
+    } catch (err) {
+        res.status(SERVER_ERR_CODE);
+        err.message = SERVER_ERROR;
+        next(err);
+    }
+    if (db) { // only defined if getDB() returned a successfully-connected object
+        db.end();
+    }
+});
+
+app.post('/admin/product/edit', async (req, res, next) => {
+    let productId = req.body.product_id;
+    let productTitle = req.body.title;
+    let productDescription = req.body.description;
+    let productPrice = req.body.price;
+    let productQty = req.body.quantity;
+    let productCategory = req.body.category;
+
+    if (!(productId && productDescription &&
+        productTitle && productPrice && productQty && productCategory)) {
+        res.status(CLIENT_ERR_CODE); // re-route to errorHandler, exiting this function
+        next(Error("Missing POST parameter: product id, title, description, " +
+                   "price, quantity, and/or category."));
+    }
+    let db;
+    try {
+        db = await getDB(); // connection error thrown in getDB();
+        let qry = "UPDATE products SET title=?, description=?, price=?, quantity=?, category=? " +
+                  "WHERE id=?;";
+        await db.query(qry, [productTitle, productDescription, productPrice,
+                             productQty, productCategory, productId]);
+        res.json({"status_message": `Request to edit product ${productId} ` +
+                 `successfully processed!`});
+    } catch (err) {
+        res.status(SERVER_ERR_CODE);
+        err.message = SERVER_ERROR;
+        next(err);
+    }
+    if (db) { // only defined if getDB() returned a successfully-connected object
+        db.end();
+    }
+});
+
+app.post('/admin/product/delete', async (req, res, next) => {
+    let productId = req.body.product_id;
+
+    if (!(productId)) {
+        res.status(CLIENT_ERR_CODE);
+        next(Error("Missing POST parameter: product id."));
+    }
+    let db;
+    try {
+        db = await getDB(); // connection error thrown in getDB();
+        let qry = "DELETE FROM products WHERE id=?;";
+        await db.query(qry, [productId]);
+        res.json({"status_message": `Request to delete product ${productId} ` +
+                 `successfully processed!`});
+    } catch (err) {
+        res.status(SERVER_ERR_CODE);
+        err.message = SERVER_ERROR;
+        next(err);
+    }
+});
+
+app.post('/admin/login', async (req, res, next) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    res.cookie("logged_in", "true", { maxAge : COOKIE_EXP_TIME });
+    res.json({
+        "success": true,
+        "status_message": "Successfully logged into admin portal!"
+    });
+});
+
+app.post('/contact', async (req, res, next) => {
+    let storeName = req.body.store_name;
+    let email = req.body.email;
+    let message = req.body.message;
+
+    if (!(storeName && message && email)) {
+        res.status(CLIENT_ERR_CODE);
+        next(Error("Missing POST parameter: store name, email, and/or message."));
+    }
+    let db;
+    try {
+        db = await getDB(); // connection error thrown in getDB();
+        let qry = "INSERT INTO contact_messages (store_name, email, msg) " +
+                  "VALUES (?, ?, ?);";
+        await db.query(qry, [storeName, email, message]);
+        res.json({"status_message": "Successfully sent message!", "success": true});
+    } catch (err) {
+        res.status(SERVER_ERR_CODE);
+        err.message = SERVER_ERROR;
+        next(err);
+    }
+});
+
+/**
+ * Error-handling middleware.
+ */
+function errorHandler(err, req, res, next) {
+    if (DEBUG) {
+      console.error(err);
+    }
+    res.json({"status_message": err.message});
+}
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT);
---
+++ setup.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE IF NOT EXISTS storedb;
+USE storedb;
+
+CREATE TABLE IF NOT EXISTS admin_accounts (
+    email VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS products (
+    id          INTEGER PRIMARY KEY AUTO_INCREMENT,
+    store_name  VARCHAR(32) NOT NULL,
+    category    VARCHAR(255) NOT NULL,
+    title       VARCHAR(64) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    price       INT NOT NULL,
+    quantity    INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS contact_messages (
+    store_name  VARCHAR(32) NOT NULL,
+    email       VARCHAR(255) NOT NULL,
+    msg         TEXT NOT NULL
+);
